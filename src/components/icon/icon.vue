1c126592f90a5348dac6db9c5c6ecf20ca9d658b
qqlcx5 <893917884@qq.com>
2020-04-13 17:14:18 +0800

    icon

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <svg class="icon" aria-hidden="true" :class="svgClass" >
+        <use :xlink:href="`#${icon}`" ></use>
+      </svg>
+  </div>
+</template>
+<script>
+export default {
+  name: 'icon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    svgClass:{
+      type: String
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  color: #999;
+}
+</style>
--- src/views/Input.vue
+++ src/views/Input.vue
@@ -1,26 +1,42 @@
 <template>
-  <div>
+  <div class="bg">
     <lcx-input v-model="input"></lcx-input>
     <lcx-input v-model="input" :disabled="true"></lcx-input>
     <lcx-input v-model="input" show-password></lcx-input>
-
-
+    <div style="display:flex;">
+      <lcx-icon icon="up" />
+       <lcx-icon icon="down" />
+    </div>
     {{ input }}
   </div>
 </template>
 <script>
 import lcxInput from '../components/input/input';
+import lcxIcon from '../components/icon/icon';
 export default {
   data() {
     return {
-      input: 'input',
+      input: 'input'
     };
   },
   methods: {
-    handleShow() {},
+    handleShow() {}
   },
   components: {
     lcxInput,
-  },
+    lcxIcon
+  }
 };
 </script>
+<style lang="scss" scoped>
+.bg {
+  font-size: 30px;
+  &::v-deep .icon{
+
+  // width: 15px;
+  // height: 30px;
+  // transform:scaleX(1.5);
+  color: #000;
+  }
+}
+</style>
