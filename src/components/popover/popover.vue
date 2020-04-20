79c636fc7a8b9cb685ff750e1e818e6818844023
qqlcx5 <893917884@qq.com>
2020-04-20 16:47:46 +0800

    popover

--- src/components/popover/popover.vue
+++ src/components/popover/popover.vue
@@ -1,25 +1,46 @@
 <template>
-  <div>
-    <div v-if="visible" ref="popover">
+  <div class="lcx-popover">
+    <div
+      class="lcx-popover-content"
+      :class="`content-${placement}`"
+      v-if="visible"
+      ref="content"
+      @click.stop
+    >
       <h3>{{ title }}</h3>
       {{ content }}
+      <i class="arrow"></i>
     </div>
     <slot name="reference"></slot>
   </div>
 </template>
 <script>
-const on = (element, event, handle) =>{
-  element.addEventListener(event, handle, false)
-}
-// const off = (element, event, handle) =>{
-//   element.removeEventListener(event, handle, false)
-// }
+const on = (element, event, handle) => {
+  element.addEventListener(event, handle, false);
+};
+const off = (element, event, handle) => {
+  element.removeEventListener(event, handle, false);
+};
 export default {
   name: 'lcx-popover',
   data() {
     return {
       visible: false,
     };
+  },
+  watch: {
+    visible(val) {
+      if (val) {
+        this.$nextTick(() => {
+          let content = this.$refs.content;
+          document.body.appendChild(content);
+          if (this.trigger === 'hover') {
+            on(content, 'mouseenter', this.handleMouseEnter);
+            on(content, 'mouseleave', this.handleMouseLeave);
+          }
+        });
+      }
+    },
   },
   props: {
     title: {
@@ -47,29 +68,114 @@ export default {
     },
   },
   methods: {
-    handleToggle(){
-      this.visible = !this.visible
+    handleToggle() {
+      this.visible = !this.visible;
     },
-    handleDocumentToggle(e){
-      if(this.$el.contains(e.target)) return
-      this.visible = false
+    handleDocumentToggle(e) {
+      if (this.$el.contains(e.target)) return;
+      this.visible = false;
     },
-    // handleToggle(){
-    //   this.visible = !this.visible
-    // },
+    handleMouseEnter() {
+      clearTimeout(this.timer);
+      this.visible = true;
+    },
+    handleMouseLeave() {
+      this.timer = setTimeout(() => {
+        this.visible = false;
+      }, 200);
+    },
   },
   mounted() {
     let reference = this.$slots.reference;
     if (reference) {
       this.reference = reference[0].elm;
     }
-    // if (this.trigger === 'hover') {
-
-    // }
-    if (this.trigger === 'click') {
-      on(this.reference, 'click', this.handleToggle)
-      on(document, 'click', this.handleDocumentToggle)
+    if (this.trigger === 'hover') {
+      on(this.$el, 'mouseenter', this.handleMouseEnter);
+      on(this.$el, 'mouseleave', this.handleMouseLeave);
+    } else if (this.trigger === 'click') {
+      on(this.reference, 'click', this.handleToggle);
+      on(document, 'click', this.handleDocumentToggle);
     }
+  },
+  beforeDestroy() {
+    off(this.$el, 'mouseenter', this.handleMouseEnter);
+    off(this.$el, 'mouseleave', this.handleMouseLeave);
+    off(this.reference, 'click', this.handleToggle);
+    off(document, 'click', this.handleDocumentToggle);
   },
 };
 </script>
+<style lang="scss" scoped>
+.lcx-popover {
+  display: inline-block;
+  position: relative;
+  &-content {
+    position: absolute;
+    top: 100px;
+    left: 30px;
+    padding: 15px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+  }
+}
+.arrow {
+  &::before {
+    position: absolute;
+    // top: -19px;
+    // left: 50%;
+    // transform: translate(-50%);
+    display: block;
+    content: '';
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-bottom-color: #fff;
+    filter: drop-shadow(0 -2px 1px #eee);
+  }
+  // &::after {
+  //   border-bottom-color: #fff;
+  //   transform: translateY(1px);
+  //   filter: drop-shadow(0 -2px 1px #ccc);
+  // }
+}
+.content-top{
+  .arrow {
+    &::before {
+      top: -19px;
+      left: 50%;
+      margin-left: -10px;
+    }
+  }
+}
+.content-bottom{
+  .arrow {
+    &::before {
+      bottom: -19px;
+      left: 50%;
+      margin-left: -10px;
+      transform: rotate(-180deg);
+    }
+  }
+}
+.content-left{
+  .arrow {
+    &::before {
+      top: 50%;
+      left: -19px;
+      margin-top: -10px;
+      transform: rotate(-90deg);
+    }
+  }
+}
+.content-right{
+  .arrow {
+    &::before {
+      top: 50%;
+      right: -19px;
+      margin-top: -10px;
+      transform: rotate(90deg);
+    }
+  }
+}
+</style>
--- src/views/Popover.vue
+++ src/views/Popover.vue
@@ -9,7 +9,17 @@
     >
       <el-button slot="reference">click 激活</el-button>
     </el-popover>
-    <!--  -->
+    <!--   -->
+    <lcx-popover
+      class="test"
+      placement="top"
+      title="标题"
+      width="200"
+      trigger="hover"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+    >
+      <lcx-button slot="reference">hover 激活</lcx-button>
+    </lcx-popover>
     <lcx-popover
       placement="top"
       title="标题"
@@ -17,7 +27,7 @@
       trigger="click"
       content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
     >
-      <el-button slot="reference">click 激活</el-button>
+      <lcx-button slot="reference">click 激活</lcx-button>
     </lcx-popover>
   </div>
 </template>
@@ -27,4 +37,9 @@ export default {
   methods: {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  // .test{
+  //   height: 30px;
+  //   overflow: hidden;
+  // }
+</style>
