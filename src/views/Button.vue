e60cd339b37669f608f990e9a72b6f87fb592b0e
qqlcx5 <893917884@qq.com>
2020-07-13 21:11:46 +0800

    comp

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,10 +1,57 @@
 <template>
   <div>
-    <lcx-button type="default">default</lcx-button>
+    <!-- <lcx-button type="default">default</lcx-button>
     <lcx-button type="primary">primary</lcx-button>
     <lcx-button type="info">info</lcx-button>
     <lcx-button type="danger">danger</lcx-button>
     <lcx-button type="success">success</lcx-button>
-    <lcx-button type="warning" icon='icon-chuangzuo'>warning</lcx-button>
+    <lcx-button type="warning" icon='icon-chuangzuo'>warning</lcx-button> -->
+    <Child @listen="parentmethods"></Child>
+
+    <h2>parentData</h2>
+    {{parentData}}
   </div>
 </template>
+
+<script>
+import Child from './Child'
+export default {
+  data() {
+    return {
+      parentData:false
+    }
+  },
+  components:{
+    Child
+  },
+  methods:{
+    parentmethods(v){
+      this.parentData = v
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate parent')
+  },
+  beforeMount() {
+    console.log('beforeMount parent')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate parent')
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy parent')
+  },
+  created(){
+    console.log('Create parent')
+  },
+  mounted(){
+   console.log('mounted parent')
+  },
+  updated(){
+    console.log('updated parent')
+  },
+  destroyed(){
+    console.log('destroyed parent')
+  }
+}
+</script>
---
+++ src/views/Child.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    ChildChildChild
+    <div @click="test = !test">
+     {{test}}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      test: false
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate Child')
+  },
+  beforeMount() {
+    console.log('beforeMount Child')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate Child')
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy Child')
+  },
+  created(){
+    console.log('Create Child')
+  },
+  mounted(){
+   console.log('mounted Child')
+  },
+  updated(){
+    console.log('updated Child')
+    this.$emit('listen', this.test)
+  },
+  destroyed(){
+    console.log('destroyed Child')
+  }
+}
+</script>
